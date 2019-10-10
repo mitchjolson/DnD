97da8fa21c2e9f7ae89fc4dd76df507a0cc81adb
unknown <mitchjolson@gmail.com>
2019-10-10 15:44:47 -0500

    able to add monster name and CR to DB

--- server/routes/monsters.router.js
+++ server/routes/monsters.router.js
@@ -3,13 +3,15 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.post('/add', async (req, res) => {
-    console.log('adding a monster, req.body is', req.body)
+    // console.log('adding a monster, req.body is', req.body)
     let connection = await pool.connect();
     try {
         await connection.query('BEGIN');
-        await connection.query('INSERT INTO monsters (name, challenge_rating) VALUES ($1, $2) returning id;',[req.body.name, req.body.challenge_rating]);
+        const monsterInsertDetails = await connection.query('INSERT INTO monsters (name, challenge_rating) VALUES ($1, $2) returning id;',[req.body.name, req.body.challenge_rating]);
+        const monsterId = monsterInsertDetails.rows[0].id;
+        console.log('the returned ID is', monsterId)
         await connection.query('COMMIT');
-        res.send(response)
+        res.sendStatus(201)
     }
     catch (error) {
         console.log(error);
